76832b1ec24e86cafe511afd7a606283c2ad7f16
Ghislain PUTOIS <ghislain.putois@gmail.com>
2020-05-19 09:34:36 +0200

    Une version presque prête, manque les contrôles de la ligne de commande.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use nvml_wrapper::NVML;
+use std::env;
 use std::thread;
+use std::time;
 use std::sync::Arc;
 use std::sync::atomic;
 use subprocess;
@@ -7,8 +9,9 @@ use subprocess;
 
 fn main() {
     println!("GPULoad monitoring");
-    let mut process = subprocess::Exec::shell("find / -type f > /dev/null 2> /dev/null").popen().unwrap();
-    let child="firefox";
+    let args: Vec<String>=env::args().collect();
+    let child = args[1].clone();
+    let mut process = subprocess::Exec::cmd(child.clone()).args(&args[2..]).popen().unwrap();
     let pid = process.pid();
 
     let started2 = Arc::new(atomic::AtomicBool::new(false));
@@ -23,39 +26,40 @@ fn main() {
         println!("{} gpus found",dc);
 
 
-        for gpu_id in (0..dc) {
+        for gpu_id in 0..dc {
 
             let device = nvml.device_by_index(gpu_id).unwrap();
             let memory_info = device.memory_info().unwrap();
             println!("{:?}",memory_info);
 
-            let acc: usize = 0;
-
-
-
             while !finished.load(atomic::Ordering::Relaxed) {
+                let mut acc: u64 = 0;
                 let processes = device.running_graphics_processes().unwrap();
 
                 let mut found = false;
                 for p in processes {
                     let name = nvml.sys_process_name(p.pid,65536).unwrap_or("".to_string()); // 65536 : max length for process name, otherwise truncated
-                    if name.contains(child) {
+                    if name.contains(&child) {
                         let already_started = started.swap(true,atomic::Ordering::Relaxed);
                         if already_started == false {
-                        println!("GPULoad started");
+                            println!("GPULoad started");
                         }
                     }
                     if started.load(atomic::Ordering::Relaxed) {
-                        if name.contains(child) {
+                        if name.contains(&child) {
                             found = true;
-                            println!("{:?}",p);
+                            acc += match p.used_gpu_memory { nvml_wrapper::enums::device::UsedGpuMemory::Used(t) => t, _ => 0};
                         }
                     }
 
                 }
-                    if found==false {
-                        finished.swap(true,atomic::Ordering::Relaxed);
-                    }
+                println!("Used memory : {}",acc);
+                if found==false {
+                    finished.swap(true,atomic::Ordering::Relaxed);
+                } else {
+                    thread::sleep(time::Duration::from_millis(1000));
+                }
+
 
             }
             println!("GPULoad finished");
